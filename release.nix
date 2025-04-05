686954d143a5cf9c5acb084e6965cfb003d36500
Aravindan M <83415736+aravindan888@users.noreply.github.com>
2025-04-05 18:16:04 +0530

    Add files via upload

---
+++ release.nix
@@ -0,0 +1,18 @@
+with import ./pynixify/nixpkgs.nix { };
+let
+  version = builtins.head (builtins.match ".*'([0-9]+.[0-9]+(.[0-9]+)?)'.*"
+    (builtins.readFile ./faraday/__init__.py));
+in { useLastCommit ? true }: rec {
+
+  faraday-server = python3.pkgs.faradaysec.overrideAttrs (old:
+    assert !builtins.hasAttr "checkInputs" old; {
+      name = "faraday-server-${version}";
+      doCheck = true;
+      checkPhase = "true";
+    } // lib.optionalAttrs useLastCommit {
+      src = builtins.fetchGit {
+        url = ./.;
+        ref = "HEAD";
+      };
+    });
+}
